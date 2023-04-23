5f5e508341223211f198148f000fe14a34d8439c
John Shrader <figurefactory@gmail.com>
2023-04-22 20:54:09 -0400

    some style fixes

--- source/Game/Game.tsx
+++ source/Game/Game.tsx
@@ -1,3 +1,13 @@
 // load or create user
 // load saved game or start new game
 // game - load curent challenge
+
+// import React from 'react';
+import {useApp, useInput} from 'ink';
+
+const { exit } = useApp();
+  useInput((_input, key) => {
+    if (key.escape) {
+      exit();
+    }
+  });
--- source/app.tsx
+++ source/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text} from 'ink';
+import {Text, Box} from 'ink';
 import BigText from 'ink-big-text';
 
 import { getUserdata } from './User/User.js';
@@ -10,13 +10,14 @@ type Props = {
 };
 
 export default function App({name = 'Stranger'}: Props) {
+	const welcomeText = `${name} welcome to`;
 	return (
 		<>
-		<Text>
-			Hello, <Text color="green">{name}</Text> welcome to:
-		</Text>
-		<BigText text="Code" colors={['#ff2500', 'gray']} font='3d'/>
-		<BigText text="Rebellion" colors={['#ff2500', 'gray']} font='3d'/>
+		<BigText text={welcomeText} font='simple3d' align='center' colors={['white', 'white']} backgroundColor='transparent'/>
+		<Box borderStyle="round" flexDirection="column">
+			<BigText text="Code" colors={['#ff2500', 'gray']} font='3d' align='center'/>
+			<BigText text="Rebellion" colors={['#ff2500', 'gray']} font='3d' align='center'/>
+		</Box>
 		<ObjectDisplay data={getUserdata()}/>
 		<Text>
 			Need a line break here::::::
